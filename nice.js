46e1999e071a8f4b95c5d297538a204e8f2a8339
NextGen45 <tommy48steele@gmail.com>
2021-07-09 13:38:46 -0700

    updated webpage files

---
+++ nice.js
@@ -0,0 +1,21 @@
+Function askquestion(){
+    var numberfound = false;
+    while (numberfound ==false){
+        var numberfound = prompt("enter a number 1 -12");
+    if (numberfound == "10"){
+    
+        alert("Great job!");
+        numberfound = true;
+    }
+    else{ alert("wrong Number my friend");
+    
+    
+    }
+    
+    }
+    
+    
+    
+    
+    }
+
