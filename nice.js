bbc9ecbb7f30eccbf532130a8b27075efd40c3cf
NextGen45 <tommy48steele@gmail.com>
2021-07-12 09:21:50 -0700

    mywebpage

--- nice.js
+++ nice.js
@@ -1,28 +1,26 @@
 function askquestion() {
-  var namefound= false;
+  var namefound = false;
   while (namefound == false) {
     var name = prompt("Which artrist does dc comics ");
     if (name == "Jim lee") {
       alert("Great job!");
       namefound = true;
-    }
-     else { 
-alert("wrong name my friend");
+    } else {
+      alert("wrong name my friend");
     }
   }
 }
 askquestion();
 
+function imageCount() {
+  var image = prompt("how many batmans do you wanna see");
+  for (i = 0; i < image; i++) {
+    document.write(
+      '<img src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/06/Batman-fighting-in-Detective-Comics-1037.jpg" >'
+    );
 
-//function imageCount(){
-   // var image = prompt("how many batmans do you wanna see");
-    //for( i = 0; i < imageCount; i++) {
-      //document.write('<img src="https://static.wikia.nocookie.net/marvel_dc/images/4/4b/Batman_Vol_3_86_Textless.jpg/revision/latest?cb=20200502132734" alt="img">');
+    console.log(i);
+  }
+}
 
-     // console.log(i); 
-    
-
-//imageCount();
-
-
-
+imageCount();
